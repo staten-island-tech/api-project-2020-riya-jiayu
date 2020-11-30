9644b2829ecc7f0524a6aae941b244bf1868669a
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-11-30 11:27:42 -0500

    （

--- src/js/index.js
+++ src/js/index.js
@@ -16,17 +16,17 @@ nextPage();
 const init = async function(pageNumber){
   const DOMSelectors = {
 };
-const key = `81e21e5440msh0b93b8e3b7cb749p116c5bjsnf5acd0869f72`;
-const query = `https://jikan.moe/`;
+const id = []
+const query = `https://jikan.moe/v3/anime/${id}/${request}/${parameter}`;
     try {
         const response = await fetch(query);
         const data = await response.json()
-        data.results.forEach((movie) => {
+        data.results.forEach((anime) => {
             
             let genreArr = [];
             const genreIds = function(){
                 genres.forEach((element) =>{
-                if (movie.genre_ids.includes(element.id)) {
+                if (anime.genre_ids.includes(element.id)) {
                     genreArr.push(element.name);
                     return genres;
                 }
@@ -36,27 +36,27 @@ const query = `https://jikan.moe/`;
             genreIds();
             DOMSelectors.grid.insertAdjacentHTML(
                 "beforeend",
-                `<div class="movie-card">
-                <div class="movie-card-front">
+                `<div class="anime-card">
+                <div class="anime-card-front">
                   <img
-                    src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+                    src="https://image.tmdb.org/t/p/w300/${anime.poster_path}"
                     alt=""
                     class="poster"
                   />
                 </div>
-                <div class="movie-card-back">
-                  <h3 class="movie-card-header">${movie.original_title}</h3>
+                <div class="anime-card-back">
+                  <h3 class="anime-card-header">${anime.original_title}</h3>
                   <div class="score-box">
                     <p class="user-score">Community Score</p>
-                    <p class="user-score">${movie.vote_average}</p>
+                    <p class="user-score">${anime.vote_average}</p>
                   </div>
         
                   <div class="release-box">
                     <p class="release-date">Released</p>
-                    <p class="release-date">${movie.release_date}</p>
+                    <p class="release-date">${anime.release_date}</p>
                   </div>
         
-                  <div class="movie-genres">
+                  <div class="anime-genres">
                     <div>${genreArr}</div>
                   </div>
                 </div>
