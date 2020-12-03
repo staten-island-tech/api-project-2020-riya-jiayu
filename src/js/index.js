015005fe5f7f07d0c3b627a08ffa973de274ff19
jchen2021 <57500286+jchen2021@users.noreply.github.com>
2020-12-03 11:44:35 -0500

    .

--- src/js/index.js
+++ src/js/index.js
@@ -28,7 +28,7 @@ const init = async function (pageNumber){
   const DOMSelectors = {
     grid: document.querySelector(".anime-grid"),
   }
-const query = `https://api.jikan.moe/v3/search/anime?q=&page=${pageNumber}&sort=desc&order_by=members&limit=12`;
+const query = `https://api.jikan.moe/v3/search/anime?q=&page=${pageNumber}&sort=desc&order_by=score&limit=12`;
     try {
         const response = await fetch(query);
         const data = await response.json()
